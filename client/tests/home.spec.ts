22b5852a76d8a65a50df2161b031a6a92f6ec396
Mathieu <mathieufleblanc@outlook.com>
2024-01-04 14:50:36 -0500

    Testing the use of the CI flag on the container

--- client/tests/home.spec.ts
+++ client/tests/home.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Testing Home page', async ({ page }) => {
+test.only('Testing Home page', async ({ page }) => {
     await page.goto('/');
 
     // Expect a title "to contain" a substring.
