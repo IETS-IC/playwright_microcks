552c1203b36c925e86ce635c819fd1544a1c9888
Mathieu <mathieufleblanc@outlook.com>
2024-01-04 15:09:55 -0500

    Removing .only suffix used to test the flag

--- client/tests/home.spec.ts
+++ client/tests/home.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.only('Testing Home page', async ({ page }) => {
+test('Testing Home page', async ({ page }) => {
     await page.goto('/');
 
     // Expect a title "to contain" a substring.
