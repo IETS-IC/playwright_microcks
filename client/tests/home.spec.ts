523563a2e477a5e7715a7863123ab0890e222bf2
Mathieu <mathieufleblanc@outlook.com>
2024-01-04 14:16:02 -0500

    Optimizing solution for missing locators

--- client/tests/home.spec.ts
+++ client/tests/home.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Testing Home page', async ({ page }) => {
-    await page.goto('http://localhost:4200/');
+    await page.goto('/');
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle('Home | Playwright & Microcks');
--- client/tests/postal-code.spec.ts
+++ client/tests/postal-code.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Testing menu', async ({ page }) => {
-    await page.goto('http://localhost:4200/');
+    await page.goto('/');
 
     await page.getByLabel('Click to open menu').click();
     await page.getByRole('button', { name: 'Postal Code' }).click();
